69cdf082443da00adb4b18ce8f734164c3eecb33
Prashant <prashant.kumar@logicoy.com>
2020-05-18 16:04:45 +0530

    added service notation to the prduct service

--- src/main/java/com/luvbrite/service/ProductService.java
+++ src/main/java/com/luvbrite/service/ProductService.java
@@ -3,10 +3,12 @@ package com.luvbrite.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.luvbrite.model.ProductsExt;
 import com.luvbrite.repository.ProductRepositoryImpl;
 
+@Service
 public class ProductService implements IProductService {
 
 	@Autowired
