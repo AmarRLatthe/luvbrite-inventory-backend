f063e2da7615a406d2c71ee6bd44fd9ab643aa1e
MAHAJAN DIVYESH <mahajan.divyesh@logicoy.com>
2020-05-18 12:48:45 +0530

    operator-roles updated by Dvs Mahajan

--- src/main/java/com/luvbrite/controller/ReturnsController.java
+++ src/main/java/com/luvbrite/controller/ReturnsController.java
@@ -1,9 +1,5 @@
 package com.luvbrite.controller;
-//TODO remove unused imports
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.luvbrite.commonResponse.CommonResponse;
 
 @RestController
 public class ReturnsController {
